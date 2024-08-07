9a07eaae8ad0927292a4e2d80bf316a8b5e7d860
Yasmin de França Ribeiro <64998808+yasmin-fribeiro@users.noreply.github.com>
2024-08-07 12:17:10 -0300

    “”

---
+++ src/components/navBar/NavBar.tsx
@@ -0,0 +1,26 @@
+import { Link, useNavigate } from 'react-router-dom'
+import React from 'react'
+
+
+function NavBar() {
+ 
+  
+
+    return (
+        <>
+          <div className='w-full bg-green-400 text-black flex justify-center py-4'>
+            <div className="container flex justify-between text-lg">
+              <div className='flex justify-center font-bold'>Kelp</div>
+                <div className='flex gap-4'>
+                  <Link to='/login' className='hover:underline'>Login</Link>
+                  <Link to='/home' className='hover:underline'>Home</Link>
+                  <Link to='/sobre' className='hover:underline'>Sobre</Link>
+                  
+                </div>
+              </div>
+            </div>
+        </>
+      )
+    }
+
+    export default NavBar
