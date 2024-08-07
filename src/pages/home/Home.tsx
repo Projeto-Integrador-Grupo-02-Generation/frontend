fbd72d96b3431bebb4245cd7e03ada8a246007ed
NotofTroy <helenacsfonseca@hotmail.com>
2024-08-07 15:44:27 -0300

    feat: adicionando conteúdo da página home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,14 +1,14 @@
 function Home() {
 
     return (
-        <div className="bg-indigo-900 flex justify-center">
-            <div className='container grid grid-cols-2 text-white'>
+        <div className="bg-green-400 flex justify-center">
+            <div className='container grid grid-cols-1 text-white'>
                 <div className="flex flex-col gap-4 items-center justify-center py-4">
                     <h2 className='text-5xl font-bold'>Seja bem vinde!</h2>
-                    <p className='text-xl'>Expresse aqui seus pensamentos e opiniões</p>
+                    <p className='text-xl'>Faça login para administrar sua conta.</p>
 
                     <div className="flex justify-around gap-4">
-                        <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver postagens</button>
+                        <button className='rounded bg-white text-blue-800 py-2 px-4'>Fazer login</button>
                     </div>
                 </div>
             </div>
